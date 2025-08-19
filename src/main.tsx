2312041b2aac1f38392f9ddb64c2a3ebddc07572
RiadAlnajjar <riad.alnajjar4@gmail.com>
2025-08-19 21:24:30 +0300

    feat: implement core application structure and routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from '@/store/store'
+import { App } from '@/App'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<div className="min-h-screen bg-gray-50">
+					<div className="container mx-auto px-4 py-8">
+						<App />
+					</div>
+				</div>
+			</BrowserRouter>
+		</Provider>
+	</StrictMode>,
 )
